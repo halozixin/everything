17b1e9416ae7d633f4247f1d5bf2d5dcc3e32a91
halozixin <975577725@qq.com>
2019-03-12 19:42:25 +0800

    初始化

---
+++ src/main/java/com/zh/everything/cmd/EveryThingCmdApp.java
@@ -0,0 +1,11 @@
+package com.zh.everything.cmd;
+
+/**
+ * @auther zh
+ * @data 2019/3/12 19:17
+ */
+public class EveryThingCmdApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/zh/everything/cmd/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * @auther zh
+ * @data 2019/3/12 19:15
+ */
+package com.zh.everything.cmd;
+
+/**
+ * cmd包中主要放置everything应用程序交互类
+ *
+ *
+ *
+ * */
+
